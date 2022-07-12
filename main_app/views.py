6f6468c1c154f35ccbedf336c08c443850e33caf
Annie Ma <anniema@Annies-MacBook-Pro.local>
2022-07-12 18:10:30 -0400

    successful addition of api onto home page

--- main_app/views.py
+++ main_app/views.py
@@ -3,14 +3,19 @@ from django.shortcuts import render
 # apiKey - xURY1NGNvFUgDmNVX1no+w==uRCSd79qlz5t4Kxs
 # Create your views here.
 def home(request):
+    import json
     import requests
+    if request.method == 'POST':
+        query = request.POST['query']
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
+        api_request = requests.get (api_url + query, headers = {'X-Api-Key': 'xURY1NGNvFUgDmNVX1no+w==uRCSd79qlz5t4Kxs'})
+        try: 
+            api = json.loads(api_request.content)
+            print(api_request.content)
 
-    query = 'apple'
-    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
-    response = requests.get(api_url, headers={'X-Api-Key': 'xURY1NGNvFUgDmNVX1no+w==uRCSd79qlz5t4Kxs'})
-    if response.status_code == requests.codes.ok:
-        print(response.text)
+        except Exception as e:
+            api = "Oops! Error"
+            print(e)
+        return render(request, 'home.html', {'api':api})
     else:
-        print("Error:", response.status_code, response.text)
-
-    return render(request, 'home.html')
+        return render(request, 'home.html', {'query': 'Enter a valid query'})
